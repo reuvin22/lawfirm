b96f6030c98d1568c2dcf58df6ff44b4b919c185
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2024-07-09 19:39:45 +0800

    Update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -23,13 +23,13 @@ function HomePage() {
             <div className='w-full h-full'>
                 <div className='sm:grid flex justify-center items-center h-full w-full sm:ml-3'>
                     <div className='w-[50%] h-full grid place-items-center'>
-                        <div className='w-96 ml-2'>
-                            <span className='font-sans text-[45px] sm:w-[90%] sm:font-bold'>How we deal with the law</span>
+                        <div className='w-96 px-2'>
+                            <span className='font-sans text-[45px] sm:w-[90%] sm:text-[38px] sm:font-bold'>How we deal with the law</span>
                             <p className='mt-5 text-xl sm:text-xl sm:w-[90%]'>Our clients' bottom line is our top priority. We are in constant communication with the people we serve and are committed to advancing the immediate needs and wants of our clients. Our team works around the clock to provide the necessary services and support that each client needs from us.</p>
                             <button className='bg-[#CDAD7D] sm:w-44 w-52 mt-5 sm:mt-7 sm:px-5 px-2 py-1 rounded-sm font-sans sm:text-[20px] text-[20px] hover:bg-[#b9af9b] cursor-pointer delay-100 transition-all'>Get in touch</button>
                         </div>
                     </div>
-                    <div className='w-[50%] sm:w-[100%] h-full'>
+                    <div className='w-[50%] sm:w-[100%] h-full sm:mb-5'>
                         <div className='flex items-center justify-center bg-blue-200 w-[80%] rounded-xl ml-10 sm:ml-5 border-2 mt-5 '>
                             <img src={Atty} alt="Attorney" className='w-96 sm:h-96'/>
                         </div>
